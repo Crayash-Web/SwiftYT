10c518bfe632a7043513c57f84eeed94e817b447
Yash Bihari <bihariyash01@gmail.com>
2021-05-28 17:10:27 +0530

    Update character

--- SwiftYT.py
+++ SwiftYT.py
@@ -6,21 +6,27 @@ import tkinter as tk
 from tkinter import * 
 from tkinter.ttk import *
 from pytube import YouTube
-from PIL import Image, ImageTk
+from PIL import ImageTk, Image
+
 
 
 root = tk.Tk()
 root.title("SwiftYT - Youtube Video Downloader")
-label = Label(root, text ="WELCOME..!!").pack()
+label = Label(root, text ="NOTE: YOU MUST HAVE AN INTERNET CONNECTION").pack()
 
 # the label for url 
-URL = Label(root, text = "Enter URL").place(x = 40, y = 60)
+URL = Label(root, text = "Enter URL").place(x = 40, y = 400)
 
-URL_input_area = Entry(root, width = 50).place(x=110, y=60)
+URL_input_area = Entry(root, width = 50).place(x=110, y=400)
 
+#placing character
 
+canvas = Canvas(root, width=1000, height=1000)      
+canvas.pack()      
+img = PhotoImage(file="charachter.png")      
+canvas.create_image(20, 20, anchor=NW, image=img)    
+canvas.place(x=850, y=50)
 
-                  
-                                          
+                                                            
 
 root.mainloop()
