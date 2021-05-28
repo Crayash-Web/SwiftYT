c5f6e6d588348b3da52947f208cde6f8c44c7142
Yash Bihari <bihariyash01@gmail.com>
2021-05-28 21:28:28 +0530

    Update SwiftYT.py

--- SwiftYT.py
+++ SwiftYT.py
@@ -5,6 +5,8 @@
 import tkinter as tk
 from tkinter import * 
 from tkinter.ttk import *
+from tkinter import filedialog
+from tkinter import messagebox
 from pytube import YouTube
 from PIL import ImageTk, Image
 
@@ -15,10 +17,12 @@ root.geometry("1280x720")
 root.title("SwiftYT - Youtube Video Downloader")
 label = Label(root, text ="NOTE: YOU MUST HAVE AN INTERNET CONNECTION").pack()
 
-# the label for url 
+# the label for url - TKINTER
 URL = Label(root, text = "Enter URL").place(x = 40, y = 350)
 
 URL_input_area = Entry(root, width = 50).place(x=130, y=350)
+
+
 
 #Video Quality
 
@@ -46,3 +50,9 @@ c.place(x=50, y=50)
 
 
 root.mainloop()
+
+#Processing URL - PYHTON
+
+str_url = str(URL)
+
+yt = YouTube(str_url)
